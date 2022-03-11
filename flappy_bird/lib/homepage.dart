399cc621371e9c63a0e6854411040b334f96ce9a
Yash Arora <yasharora9416@gmail.com>
2022-03-11 20:09:44 +0530

    Added Gravity

--- flappy_bird/lib/homepage.dart
+++ flappy_bird/lib/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flappy_bird/bird.dart';
 import 'package:flutter/material.dart';
 
@@ -9,11 +11,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double birdYaxis = 0;
+  static double birdYaxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = birdYaxis;
+  double velocity = 2.8;
+  bool isGameRunning = false;
+
   void jump() {
     setState(() {
-      birdYaxis = -0.1;
+      time = 0;
+      initialHeight = birdYaxis;
     });
+  }
+
+  void startGame() {
+    isGameRunning = true;
+    Timer.periodic(
+      const Duration(milliseconds: 60),
+      (timer) {
+        time += 0.04;
+        height = -4.9 * time * time + velocity * time;
+        setState(() {
+          birdYaxis = initialHeight - height;
+        });
+        if (birdYaxis > 1) {
+          timer.cancel();
+          isGameRunning = false;
+        }
+      },
+    );
   }
 
   @override
@@ -24,17 +51,64 @@ class _HomePageState extends State<HomePage> {
           Expanded(
               flex: 2,
               child: GestureDetector(
-                onTap: jump,
+                onTap: () {
+                  if (isGameRunning) {
+                    jump();
+                  } else {
+                    startGame();
+                  }
+                },
                 child: AnimatedContainer(
                   alignment: Alignment(0, birdYaxis),
-                  duration: Duration(milliseconds: 0),
+                  duration: const Duration(milliseconds: 0),
                   child: MyBird(),
                   color: Colors.blue,
                 ),
               )),
+          Container(
+            height: 15,
+            color: Colors.green,
+          ),
           Expanded(
               child: Container(
-            color: Colors.green,
+            color: Colors.brown,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Score",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      '0',
+                      style: TextStyle(color: Colors.white, fontSize: 35),
+                    )
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      "Best Score",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      '10',
+                      style: TextStyle(color: Colors.white, fontSize: 35),
+                    )
+                  ],
+                )
+              ],
+            ),
           ))
         ],
       ),
